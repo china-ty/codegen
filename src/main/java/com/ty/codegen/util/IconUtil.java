ce78101cb97be263b201dbad76a6fc84d0af492f
ty <test@163.com>
2021-01-21 14:26:55 +0800

    1.图标加载统一使用工具类 2.将项目可以打成jar包运行 3.删除原lib包下jar包资源使用maven下周依赖

--- src/main/java/com/ty/codegen/util/IconUtil.java
+++ src/main/java/com/ty/codegen/util/IconUtil.java
@@ -1,6 +1,11 @@
 package com.ty.codegen.util;
 
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.core.io.resource.ResourceUtil;
+import sun.swing.ImageIconUIResource;
+
 import javax.swing.*;
+import java.io.InputStream;
 
 /**
  * @Project: codegen
@@ -12,12 +17,25 @@ import javax.swing.*;
  **/
 public class IconUtil {
 
-    private IconUtil(){}
+    // 图片文件根路径
+    private static final String ROOT_PATH = "picture/";
 
-    private static Icon load(String path) {
-        return new ImageIcon(path);
+    private IconUtil() {
     }
 
-    public static final Icon MYSQL = load("src/com/ty/codegen/img/mysql.png");
-    public static final Icon TABLE = load("src/com/ty/codegen/img/table.png");
+    /**
+     * 加载图片资源
+     * @param path
+     * @return
+     */
+    private static Icon load(String path) {
+        // 由于可能会使用打包的方式运行程序,所以使用流的方式获取文件
+        InputStream fileStream = ResourceUtil.getStream(ROOT_PATH + path);
+        // 将文件流转为byte数组
+        byte[] fileByte = IoUtil.readBytes(fileStream);
+        return new ImageIconUIResource(fileByte);
+    }
+
+    public static final Icon MYSQL = load("mysql.png");
+    public static final Icon TABLE = load("table.png");
 }
